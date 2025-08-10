881fe149fcf7862a1e60c5751da55834ee111641
Shivanand Ulgulwad <shivaulgulwad5@gmail.com>
2025-08-10 19:43:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-// const quote = document.getElementById("quote");
-// const author = document.getElementById("author");
-// const nextbtn = document.getElementById("next-btn");
 
-
-
-// async function getquotes(params) {
-    
-//     let response = await fetch("https://api.quotable.io/random");
-//     let data = await response.json();
-//     quote.innerHTML = data.content;
-//     author.innerHTML = data.author;
-    
-   
-
-// }
 
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
@@ -32,10 +17,9 @@ async function getquotes() {
   }
 }
 
-// Load the first quote when page loads
 getquotes();
 
-// Load a new quote when button is clicked
 nextbtn.addEventListener("click", getquotes);
+
 
 
